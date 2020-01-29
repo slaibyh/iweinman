bcb55539156384d28761dd9bbd8053bacaaa8cb1
IanWeinman <59618917+IanWeinman@users.noreply.github.com>
2020-01-29 01:40:37 -0500

    Update climate.m

--- problem4/climate.m
+++ problem4/climate.m
@@ -1,15 +1,13 @@
-%Use 'Import Data' Button in the home tab to load in 'GlobalCarbonBudget2018' and 'GlobalTempbyYear
-GCB = xlsread('GlobalCarbonBudget2018.xlsx','Global Carbon Budget');
-GTBY = textread('GlobalTempbyYear.txt');
-%Make 'GCB' and GTBY tables
-GCB = array2table(GCB);
-GTBY = array2table(GTBY)
+%Import 'GlobalCarbonBudget.xlsx' & 'GlobalTempYear.txt' as tables
+GCB = array2table(xlsread('GlobalCarbonBudget2018.xlsx','Global Carbon Budget'));
+GTBY = array2table(textread('GlobalTempbyYear.txt'));
 %'array2table' taken from https://www.mathworks.com/matlabcentral/answers/126422-how-to-convert-double-precision-array-to-table
-%The first year GCB is 1959 but in the question it says 1850?
 %Use 'cumsum' to find the cumulative sum of 'GCB2' and 'GC3'
-CS = cumsum(GCB.GCB2 & GCB.GCB3);
+CS = cumsum(GCB.Var2 & GCB.Var3);
 %Create a table for the last 59 rows of 'GTBY2', 'GTBY11', and 'GTBY12' 
-NGTBY= table(GTBY.GTBY2(111:169),GTBY.GTBY11(111:169), GTBY.GTBY12(111:169));
-newtable = table(GCB.GCB2, GCB.GCB3, CS, NGTBY.Var1, NGTBY.Var2, NGTBY.Var3);
+NGTBY= table(GTBY.Var2(111:169),GTBY.Var11(111:169), GTBY.Var12(111:169));
+newtable = table(GCB.Var2, GCB.Var3, CS, NGTBY.Var1, NGTBY.Var2, NGTBY.Var3);
+
+%The first year in GCB is 1959 but in the question it says 1850?
 
 
