622e4506ac44854fd4cfd532103e6a152580e70e
IanWeinman <59618917+IanWeinman@users.noreply.github.com>
2020-01-30 16:29:47 -0500

    Update satellites.m

--- satellites.m
+++ satellites.m
@@ -1,18 +1,18 @@
 %Input
-T= input("minutes"); 
-%Define the constants:radius of Earth,graviational constant,mass of Earth
-R=6.371E6;
-G=6.67E-11;
-M=5.98E24;
+T = input("minutes"); 
+%Define the constants: radius of Earth, graviational constant, mass of Earth
+R = 6.371E6; 
+G = 6.67E-11;
+M = 5.98E24;
 %Convert to seconds
-T=T*60;
-h=((G*M*T^2)/(4*pi^2))^(1/3)-R; %in meters
+T = T*60;
+h = ((G*M*T^2)/(4*pi^2))^(1/3)-R; %in meters
 %Convert h to kilometers
-h=h/1000;
+h = h/1000;
 %New R is the radius of Earth plus the height of the satellite
-R=R+h;
+R = R+h;
 %Calculating the velocity of a satellite at a given period
-v=(2*pi*R)/T;
+v = (2*pi*R)/T;
 disp(round(v))
 disp(round(h))
 
