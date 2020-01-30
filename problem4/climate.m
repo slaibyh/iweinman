9da4fced314e4413fcc05c3a343fed8a91b1de65
IanWeinman <59618917+IanWeinman@users.noreply.github.com>
2020-01-30 16:29:52 -0500

    Update climate.m

--- problem4/climate.m
+++ problem4/climate.m
@@ -1,13 +1,14 @@
+%1)
 %Import 'GlobalCarbonBudget.xlsx' & 'GlobalTempYear.txt' as tables
-GCB = array2table(xlsread('GlobalCarbonBudget2018.xlsx','Global Carbon Budget'));
-GTBY = array2table(textread('GlobalTempbyYear.txt'));
+GlobCarbBud = array2table(xlsread('GlobalCarbonBudget2018.xlsx','Historical Budget'));
+GlobCarbBud.Properties.VariableNames = {'Year' 'ffai' 'luce' 'ag' 'os' 'ls' 'bi'};
+GlobTempByYear = array2table(textread('GlobalTempbyYear.txt'));
+GlobTempByYear.Properties.VariableNames = {'Year' 'AvgTemp' '3' '4' '5' '6' '7' '8' '9' '10' 'lower bound' 'upper bound'};
 %'array2table' taken from https://www.mathworks.com/matlabcentral/answers/126422-how-to-convert-double-precision-array-to-table
-%Use 'cumsum' to find the cumulative sum of 'GCB2' and 'GC3'
-CS = cumsum(GCB.Var2 & GCB.Var3);
-%Create a table for the last 59 rows of 'GTBY2', 'GTBY11', and 'GTBY12' 
-NGTBY= table(GTBY.Var2(111:169),GTBY.Var11(111:169), GTBY.Var12(111:169));
-newtable = table(GCB.Var2, GCB.Var3, CS, NGTBY.Var1, NGTBY.Var2, NGTBY.Var3);
 
-%The first year in GCB is 1959 but in the question it says 1850?
+%2)
+%Use 'cumsum' to find the cumulative sum of 'GCB2' and 'GCB3'
+CumSum = cumsum(GlobCarbBud.ffai(101:268) & GlobCarbBud.luce(101:268));
 
-
+%3)
+%Create a new table for with fossil fuel and industry emissions, land-use change emissions, the cumulative sum of carbon emissions, the average global temperature, and the lower and upper bounds of the 95% confidence interval of the combined effects
