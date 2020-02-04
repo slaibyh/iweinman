9d2feafae6ab736c79e375e0920a48986cb1845a
IanWeinman <59618917+IanWeinman@users.noreply.github.com>
2020-02-04 13:23:51 -0500

    Update projectile.m

--- projectile.m
+++ projectile.m
@@ -1,1 +1,5 @@
 function [v,a] = projectile(r,t)
+%This function has two inputs: initial velocity and initial launch angle.
+%The two outputs are the distance traveled by the projectile and time lapsed traveling that distance.
+%Define some constants
+g = 9.81;
