0088b5f1ce98a595713e36624e26cd21582c4c58
IanWeinman <59618917+IanWeinman@users.noreply.github.com>
2020-02-10 20:38:59 -0500

    Update satellites.m

--- satellites.m
+++ satellites.m
@@ -1,23 +1,36 @@
-%Input
-T = input("minutes",'s'); 
-if ~isnumeric(T)
-    error('Enter a number')
-end
 %Define the constants: radius of Earth, graviational constant, mass of Earth
 R = 6.371E6; 
 G = 6.67E-11;
 M = 5.98E24;
-%Convert to seconds
-T = T*60;
-h = ((G*M*T^2)/(4*pi^2))^(1/3)-R; %in meters
-%Convert h to kilometers
-h = h/1000;
-%New R is the radius of Earth plus the height of the satellite
-R = R+h;
-%Calculating the velocity of a satellite at a given period
-v = (2*pi*R)/T;
-disp(round(v))
-disp(round(h))
-if round(h)<0
-    error('Orbit is underground!')
+%Ask for the period with units and make it a string
+I = input("Input orbital period in 'seconds' 'minutes' 'hours' or 'days'", 's'); 
+%Split 'I' so 
+SplitI = split(I);
+Units = SplitI{2};
+T = str2num(SplitI{1});
+%Convert 'Units' to seconds
+switch Units
+    case 'seconds'
+        T = T;
+    case 'minutes'
+        T = T*60;
+    case 'hours'
+        T = T*3600;
+    case 'days'
+        T = T*86400;
+    otherwise
+        error('Please input a period with units'); %in case someone inputs a period without units or with the wrong units
 end
+
+%Calculate orbital height and velocity
+h = round((((G*M*T.^2)/(4*pi^2)).^(1/3)-R)./1000); %in kilometers
+v = round((2*pi*R)/T); %in meters/second
+
+disp(h);
+disp(v);
+
+%Check if 'h' is less than 0 aka below the surface. If it is, ask for another input
+if h <= 0
+    error("Height should not be less than zero. Please try another period")
+end
+
