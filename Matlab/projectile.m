0cfd1b5c2aeff1ddde27e32ecafed4b6ae7e62c6
IanWeinman <59618917+IanWeinman@users.noreply.github.com>
2020-02-14 02:02:13 -0500

    Update projectile.m

--- Matlab/projectile.m
+++ Matlab/projectile.m
@@ -1,20 +1,41 @@
-function [v,a] = projectile(r,t)
-%This function has two inputs: initial velocity and initial launch angle.
+function [p,t] = projectile(iv,la,ih)
+%This function has three default inputs: initial velocity (m/s or mph) and initial launch angle (degrees).
 %The two outputs are the distance traveled by the projectile and time lapsed traveling that distance.
 %Define the gravitational constant. We will use this as acceleration
 g = -9.81;
 
-%Ask for inputs for 'v' and 'a'
-v = input('initial velocity?');
-a = input('launch angle?');
-
-%Find the x and y components of initial velocity
-xvel = v*cos(a);
-yvel = v*sin(a);
-
-%Define an equation for time. Since we know the final velocity in the x-direction will be 0, then time is:
-t = -xvel/g;
-
-%Now that we have 't' we can find the distance traveled by the projectile
-r = xvel*t+0.5*g*t^2;
+switch nargin
+    case 0
+        iv=50
+        la=50
+        ih=0
+    case 1
+        la=50
+        ih=0
+    case 2
+        ih=0
+        if la <= 0 || la >= 90
+    error('Please input an angle between 0 and 90 degrees')
 end
+    case 3
+        iv=iv
+        la=la
+         if la <= 0 || la >= 90
+    error('Please input an angle between 0 and 90 degrees')
+end
+        ih=ih
+        if ih <=0
+            error('Please input a proper intitial height')
+        end
+    otherwise
+        error("Please input valid arguments")
+end
+
+xvel = iv*cosd(la)
+yvel = iv*sind(la)
+t = (-xvel/g)
+p = (xvel*t)+(0.5*g*(t^2))
+
+
+end
+
