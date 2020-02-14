ba55bd8a7e6426bc79eb8365656b8a25585df1e4
IanWeinman <59618917+IanWeinman@users.noreply.github.com>
2020-02-14 02:04:53 -0500

    Create problem4.m

---
+++ Matlab/problem4/problem4.m
@@ -0,0 +1,38 @@
+% Starting point for Problem 4
+load('smallperiodictable')
+
+%1)
+%Define a variable for all elements that have a density greater than water
+d=density > 1;
+%Find which elements' densities are greater than 1
+D=find(d);
+%Display the names of the elements defined by "D"
+names(D);
+%Count all the elements in "D"
+numel(D);
+
+%2)
+%Define a variable for all elements with over 100 isotopes
+i=isotopes>100;
+%Find which elements have over 100 isotopes
+I=find(i);
+%Display the names of the elements defined by "I"
+names(I);
+%Count all the elements in "I"
+numel(I);
+
+%3)
+%Define a variable for all elements that were discovered before 1900
+y=discyear<1900;
+%Find which elements were discovered before 1900
+Y=find(y);
+%Display the names of the elements defined by "Y"
+names(Y);
+%Count all the elements in "Y"
+numel(Y);
+
+%4)
+p = plot(atomicweight,density);
+%There does not appear to be a clear relationship between atomic weight and
+%density. 
+
