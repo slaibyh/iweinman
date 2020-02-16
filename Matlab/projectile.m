5dde425aa98b07905a7cce88716845b3355c7ad3
IanWeinman <59618917+IanWeinman@users.noreply.github.com>
2020-02-16 08:03:14 -0500

    Update projectile.m

--- Matlab/projectile.m
+++ Matlab/projectile.m
@@ -1,41 +1,46 @@
-function [p,t] = projectile(iv,la,ih)
+function [r,t] = projectile(iv,la,h)
 %This function has three default inputs: initial velocity (m/s or mph) and initial launch angle (degrees).
 %The two outputs are the distance traveled by the projectile and time lapsed traveling that distance.
 %Define the gravitational constant. We will use this as acceleration
-g = -9.81;
+g = 9.81;
 
-switch nargin
-    case 0
-        iv=50
-        la=50
-        ih=0
-    case 1
-        la=50
-        ih=0
-    case 2
-        ih=0
-        if la <= 0 || la >= 90
-    error('Please input an angle between 0 and 90 degrees')
-end
-    case 3
-        iv=iv
-        la=la
-         if la <= 0 || la >= 90
-    error('Please input an angle between 0 and 90 degrees')
-end
-        ih=ih
-        if ih <=0
-            error('Please input a proper intitial height')
-        end
-    otherwise
-        error("Please input valid arguments")
+if nargin == 0
+    iv = 15;
+    la = 30;
+    h = 0;
+elseif nargin == 1
+    la = 40
+    h = 0
+elseif nargin == 2
+    h = 0
+    if la <= 0 || la >= 90
+        error('Please input an angle between 0 and 90 degrees')
+    end
+else 
+    if h <= 0
+        error('Please input a positive value for height')
+    end
 end
 
-xvel = iv*cosd(la)
-yvel = iv*sind(la)
-t = (-xvel/g)
-p = (xvel*t)+(0.5*g*(t^2))
+r = iv*cosd(la)*(iv*sind(la)+sqrt(iv*sind(la)+2*g*h))/g
+t = (iv*sind(la) + sqrt((iv*sind(la))^2+2*g*h))/g
 
 
+stepsize = t/100;
+time = [0:stepsize:t];
+xaxis = iv*cosd(la)*time;
+yaxis = h + iv*time-.5*g*time.^2;
+
+title('Trajecotry of Projectile')
+ylabel('height (m)')
+xlabel('distance (m)')
+ylim([0 inf])
+xlim([0 inf])
+grid on
+
+motion = animatedline;
+for i = 1:length(time)
+    addpoints(motion,xaxis(i),yaxis(i))
+    drawnow %I can't firgure out why this only draws the upwards motion  
 end
-
+end
