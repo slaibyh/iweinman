8eef24348b33ba58042d2105ef60e223b74eeece
IanWeinman <59618917+IanWeinman@users.noreply.github.com>
2020-02-20 16:30:53 -0500

    Update projectile.m

--- Matlab/projectile.m
+++ Matlab/projectile.m
@@ -4,33 +4,50 @@ function [r,t] = projectile(iv,la,h)
 %Define the gravitational constant. We will use this as acceleration
 g = 9.81;
 
+%Define initial conditions if the function is run without any inputs
 if nargin == 0
-    iv = 15;
+    iv = '15 m/s';
     la = 30;
     h = 0;
 elseif nargin == 1
-    la = 40
-    h = 0
+    la = 40;
+    h = 0;
 elseif nargin == 2
-    h = 0
+    h = 0;
     if la <= 0 || la >= 90
-        error('Please input an angle between 0 and 90 degrees')
+        error('Please input an angle between 0 and 90 degrees') %Make sure that the angle is between 0 and 90 degrees
     end
 else 
     if h <= 0
-        error('Please input a positive value for height')
+        error('Please input a positive value for height') %Make sure that the height is greater than 0
     end
 end
 
-r = iv*cosd(la)*(iv*sind(la)+sqrt(iv*sind(la)+2*g*h))/g;
-t = 2*(iv*sind(la) + sqrt((iv*sind(la))^2+2*g*h))/g;
+%Make sure that the units are in m/s for the calculations
+splitiv = split(iv);
+units = splitiv{2};
+switch units
+    case 'm/s'
+        iv = iv;
+    case 'mph'
+        iv = iv/2.237 %conversion factor taken from Google
+end
 
+%Since we made iv a string we need to define a variable for the numeric part of it
+ivspeed = splitiv{1};
+speed = str2num(ivspeed);
 
+%Equations for range and time
+r = speed*cosd(la)*(speed*sind(la)+sqrt(speed*sind(la)+2*g*h))/g;
+t = 2*(speed*sind(la) + sqrt((speed*sind(la))^2+2*g*h))/g;
+
+%Define the axes for the plot of the projectile
 stepsize = t/1000;
 time = [0:stepsize:t];
-xaxis = iv*cosd(la)*time;
-yaxis = h + iv*time-.5*g*time.^2;
+xaxis = speed*cosd(la)*time;
+yaxis = h + speed*time-.5*g*time.^2;
 
+%Make sure the title, axes, and axes labels are good
 title('Trajecotry of Projectile')
 ylabel('height (m)')
 xlabel('distance (m)')
@@ -38,9 +55,10 @@ ylim([0 inf])
 xlim([0 inf])
 grid on
 
+%Make the animation for the projectile
 motion = animatedline;
 for i = 1:length(time)
     addpoints(motion,xaxis(i),yaxis(i))
-    drawnow %I can't firgure out why this only draws the upwards motion  
+    drawnow   
 end
 end
