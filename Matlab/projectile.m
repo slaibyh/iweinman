678a2b4c4dc2aa7c0c4a444affc87506947f7e5c
IanWeinman <59618917+IanWeinman@users.noreply.github.com>
2020-02-25 13:09:02 -0500

    Update projectile.m

--- Matlab/projectile.m
+++ Matlab/projectile.m
@@ -1,4 +1,4 @@
-function [r,t] = projectile(iv,la,h)
+function [r,t] = projectile(iv,units,la,h)
 %PROJECTILE can be run with 0-3 arguments. The three possible
 %arguments it takes are initial velocity (in m/s or mph), launch angle (in
 %degrees), and height (in meters). It calculates the range and the time
@@ -8,26 +8,25 @@ g = 9.81;
 
 %Define initial conditions if the function is run without any inputs
 if nargin == 0
-    iv = '15 m/s';
+    iv = 15;
+    units = 'm/s';
     la = 30;
     h = 0;
 elseif nargin == 1
+    units = 'm/s';
     la = 30;
     h = 0;
 elseif nargin == 2
+    la = 30;
     h = 0;
-    if la <= 0 || la >= 90
-        error('Please input an angle between 0 and 90 degrees') %Make sure that the angle is between 0 and 90 degrees
-    end
+elseif nargin == 3
+    h = 0;
 else 
     if h <= 0
         error('Please input a positive value for height') %Make sure that the height is greater than 0
     end
 end
 
-%Make sure that the units are in m/s for the calculations
-splitiv = split(iv);
-units = splitiv{2};
 switch units
     case 'm/s'
         iv = iv;
@@ -35,24 +34,17 @@ switch units
         iv = iv/2.237; %conversion factor taken from Google
 end
 
-%Since we made iv a string we need to define a variable for the numeric part of it
-ivspeed = splitiv{1};
-speed = str2num(ivspeed);
 
-%Make sure the input for intital velocity is a number
-if isempty(speed)
-    error('Please input a numeric value for velocity')
-end
 
 %Equations for range and time
-r = speed*cosd(la)*(speed*sind(la)+sqrt(speed*sind(la)+2*g*h))/g;
-t = 2*(speed*sind(la) + sqrt((speed*sind(la))^2+2*g*h))/g;
+r = iv^2*sind(2*la)/g;
+t = 2*(2*iv*sind(la))/g;
 
 %Define the axes for the plot of the projectile
 stepsize = t/1000;
 time = [0:stepsize:t];
-xaxis = speed*cosd(la)*time;
-yaxis = h + speed*time-.5*g*time.^2;
+xaxis = iv*cosd(la)*time;
+yaxis = h + iv*time-.5*g*time.^2;
 
 %Make sure the title, axes, and axes labels are good
 title('Trajecotry of Projectile')
