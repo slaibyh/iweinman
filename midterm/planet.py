c65230a480ad57ef5a3c9db15fa120c353f1090e
IanWeinman <59618917+IanWeinman@users.noreply.github.com>
2020-02-27 16:31:09 -0500

    Create planet.py

---
+++ midterm/planet.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+This file contains functions related to calculating orbital quantities of satellites
+"""
+import numpy as np
+import matplotlib.pyplot as pyp
+
+
+def planet(m,r,ms,a):
+    
+    T = 365
+    g = 9.8
+    
+    return T,g
+
+
+
+
+    
