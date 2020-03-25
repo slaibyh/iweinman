7c5a8e544f49e1632ba4dc0cd160e4360c543d97
IanWeinman <59618917+IanWeinman@users.noreply.github.com>
2020-03-25 18:35:10 -0400

    Update problem7a.py

--- Python/Problem 7/problem7a.py
+++ Python/Problem 7/problem7a.py
@@ -1,5 +1,27 @@
 import pandas as pd
+import numpy as np
+
+Years = np.arange(1850,2018)
+
+#Import 'GlobalTempbyMonth.txt' and 'climate.txt'
+#Globtemp = pd.DataFrame(pd.read_table('GlobalTempbyMonth.txt',usecols=[0,1],dtype=str))
+climate = pd.DataFrame(pd.read_table('climate.txt',sep=','))
+
+#Add a column containing the amount of carbon added every year and a year column
+climate['YearlyEmissions'] = climate.ffai+climate.luce
+climate['Year'] = Years
 
 
-Globtemp = pd.DataFrame(pd.read_table('GlobalTempbyMonth.txt',sep="   ",header=None, usecols = [0, 1], names=['date', 'avgtemp']))
-#clim = pd.DataFrame(pd.read_table('climate.txt',sep=','))
+#1 and 2) This function returns the hottest ten years or the 10 years with the most carbon based on the input
+def topten(str):
+    if str == 'Years':
+        sortedclimate = climate.sort_values('AvgTemp',ascending=False)
+        sctopten = sortedclimate.head(10)
+        topten = sctopten.Year
+    elif str == 'Carbon':
+        sortedclimate = climate.sort_values('YearlyEmissions',ascending=False)
+        sctopten = sortedclimate.head(10)
+        topten = sctopten.Year
+    else:
+        print("Please input 'Years' or 'Carbon'")
+    return topten
