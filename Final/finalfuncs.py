266de7bb20630a517c3aca95e31c0b03df3fa429
IanWeinman <59618917+IanWeinman@users.noreply.github.com>
2020-04-05 23:03:30 -0400

    Update finalfuncs.py

--- Final/finalfuncs.py
+++ Final/finalfuncs.py
@@ -1,4 +1,4 @@
-#Final project program
+#Final project functions program
 import numpy as np
 import tkinter as tk
 
@@ -6,31 +6,57 @@ G = 6.67*10**(-11)#gravitational constant
 M = 5.98*10**(24)#mass of Earth
 R = 6.371*10**(6)#radius of Earth
 
-#function to converts T into seconds
-def tconvert(T,str):
-    if str == 'secs':
-        T = T
-    elif str == 'mins':
-        T = T*60
-    elif str == 'hours':
-        T = T*3600
-    elif str == 'days':
-        T = T*86400
-    return T
-
-#function that converts seconds into minutes
-def secs2mins(x):
-    mins = x/60
-    return mins
-
-#function that converts between meters to kilometers
-def distconvert(x,str):
-    if str == 'm':
-        out = x*1000
-    if str == 'km':
+#function for all unit conversions
+def distconvert(x,unit,newunit):
+    if unit == 'm' and newunit == 'km':
         out = x/1000
+    if unit == 'm' and newunit == 'mi':
+        out = x/1609
+    if unit == 'm' and newunit == 'ft':  
+        out = x*3.281
+    if unit == 'km' and newunit == 'm':
+        out = x*1000
+    if unit == 'km' and newunit == 'mi':
+        out = x/1.609
+    if unit == 'km' and newunit == 'ft':
+        out = x*3281
+    if unit == 'mi' and newunit == 'm':
+        out = x*1609
+    if unit == 'mi' and newunit == 'km':
+        out = x*1.609
+    if unit == 'mi' and newunit == 'ft':
+        out = x*5280
+    if unit == 'ft' and newunit == 'm':
+        out = x/3.281
+    if unit == 'ft' and newunit == 'km':
+        out = x/3281
+    if unit == 'ft' and newunit == 'mi':
+        out = x/5280  
+    if unit == 'secs' and newunit == 'mins':
+        out = x/60
+    if unit == 'secs' and newunit == 'hrs':
+        out = x/3600   
+    if unit == 'secs' and newunit == 'days':
+        out = x/86400
+    if unit == 'mins' and newunit == 'secs':
+        out = x*60
+    if unit == 'mins' and newunit == 'hrs':
+        out = x/60
+    if unit == 'mins' and newunit == 'days':
+        out = x/1440
+    if unit == 'hrs' and newunit == 'secs':
+        out = x/3600
+    if unit == 'hrs' and newunit == 'mins':
+        out = x/60
+    if unit == 'hrs' and newunit == 'days':
+        out = x/24
+    if unit == 'days' and newunit == 'secs':
+        out = x*86400
+    if unit == 'days' and newunit == 'mins':
+        out = x*1440
+    if unit == 'days' and newunit == 'hrs':
+        out = x*24    
     return out
-
 
 #function for orbital radius
 def orbitalr(T):
@@ -56,7 +82,3 @@ def window():
     win.geometry('700x700')
     win.mainloop()
     return win
-
-
-
-
